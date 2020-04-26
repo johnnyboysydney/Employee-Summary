27fff01e8d06849ff74e9b3ba5d97ab8d5ce44cf
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-04-26 13:24:12 +1000

    adding app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");

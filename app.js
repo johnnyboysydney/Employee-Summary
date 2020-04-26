97d7d1f0ddf1019e4bc76e5150eeff3aadb212f7
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-04-26 13:35:52 +1000

    global add

--- app.js
+++ app.js
@@ -42,5 +42,102 @@ inquirer
         }        
     });
 }
+function addManager() {
+    inquirer
+        .prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: "What is the manager's ID?"
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is you manager's Email"
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is your office number?",            
+        }
+    ])
+    .then(answers => {
+        console.log(answers);
+        const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
+        employeeList.push(manager);
+        start();
+    });
+}
+function addEngineer() {
+    inquirer
+        .prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "engineerID",
+            message: "What is the engineer's ID?"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is you engineer's Email"
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the engineer's gitHub?"
+        }
+    ])
+    .then(answers => {
+        console.log(answers);
+        const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+        employeeList.push(engineer);
+        start();
+    });
+}
+function addIntern() {
+    inquirer
+        .prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "What is the intern's ID?"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is you intern's Email"
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is the intern's school?"
+        }
+    ])
+    .then(answers => {
+        console.log(answers);
+        const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+        employeeList.push(intern);
+        start();
+    });
+}
+start(); 
 
-
+function end() {
+     console.log("End");
+     fs.writeFileSync(outputPath, render(employeeList), "utf-8");
+}

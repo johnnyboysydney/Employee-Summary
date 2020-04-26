7ae04723f49e0146ef5f1a36dc9d164c5c159b7d
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-04-26 13:31:08 +1000

    working on app.js

--- app.js
+++ app.js
@@ -4,8 +4,43 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const employeeList = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+function start() {
+inquirer    
+    .prompt([
+    {
+        type: "list",
+        name: "jobType",
+        message: "What type of employee are you?",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern",
+            "list complete"            
+        ]
+    }
+
+    ])
+    .then(answers => {
+        if(answers.jobType === "Engineer"){
+            addEngineer();
+        } 
+        else if(answers.jobType === "Manager"){
+            addManager();
+        }
+        else if(answers.jobType === "Intern"){
+            addIntern();
+        }
+        else{
+            end();
+        }        
+    });
+}
+
+
